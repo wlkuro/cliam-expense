5ee31968bc17908c620aeef5903114b57b32dcd0
futeri839 <e.futami.jw@gmail.com>
2020-09-04 16:05:59 +0900

    完了画面 モック完成

--- pages/form/completed.vue
+++ pages/form/completed.vue
@@ -1,5 +1,19 @@
 <template>
-    
+  <v-container fluid>
+      <v-card class="pa-2 ma-0">
+        <v-card-title class="headline">
+          申請種別 - 完了
+        </v-card-title>
+        <v-card-text>
+          <v-row justify="center">
+            <v-col cols="12">
+              申請しました。
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+      <v-btn class="my-5" outlined to="/" nuxt>一覧へ戻る</v-btn>
+  </v-container>
 </template>
 <script>
 export default {
