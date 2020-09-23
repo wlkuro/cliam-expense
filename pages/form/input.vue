0ab30d8418a17cfa26162d0416218b79d27cf0b4
futeri839 <eri.f9107@gmail.com>
2020-09-23 19:26:31 +0900

    途中 コンポーネントの適用

--- pages/form/input.vue
+++ pages/form/input.vue
@@ -21,15 +21,7 @@
         </v-row>
         <v-row>
           <v-col>
-            <v-text-field
-              label="利用日"
-              name="moving-date"
-              value=""
-              v-bind:hint="this.exsampleDate"
-              persistent-hint
-              outlined
-            >
-            </v-text-field>
+            <DatePicker></DatePicker>
           </v-col>
         </v-row>
         <v-row class="mb-5">
@@ -76,6 +68,16 @@
         </v-row>
         <v-row>
           <v-col>
+            <v-text-field
+            label="経由駅"
+            outlined
+            hide-details
+            >
+            </v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
             <v-checkbox
               label="往復料金を出す"
               v-model="roundTrip"
@@ -90,36 +92,7 @@
       </v-card-actions>
   </v-card>
   <template v-if="this.routeFlg">
-    <v-card class="mt-2">
-      <v-card-title class="pb-0">
-        検索結果
-      </v-card-title>
-      <v-card-text>
-        <v-list two-line>
-          <v-list-item-group>
-            <template v-for="(item, index) in items">
-              <v-list-item 
-                :key="item.price"
-                to="/form/confirm"
-                nuxt
-              >
-                <template>
-                  <v-list-item-content>
-                    <v-list-item-title v-text="item.price"></v-list-item-title>
-                    <v-list-item-subtitle class="text--primary" v-text="item.route"></v-list-item-subtitle>
-                    <v-list-item-subtitle v-text="item.via"></v-list-item-subtitle>
-                  </v-list-item-content>
-                </template>
-              </v-list-item>
-              <v-divider
-                v-if="index + 1 < items.length"
-                :key="index"
-              ></v-divider>
-            </template>
-          </v-list-item-group>
-        </v-list>
-      </v-card-text>
-    </v-card>
+    <SearchRoute></SearchRoute>
   </template>
   </v-form>
   
@@ -127,37 +100,27 @@
 </v-container>
 </template>
 <script>
+import DatePicker from "~/components/DatePicker.vue"
+import SearchRoute from "~/components/search-routes.vue"
 
 export default {
     components: {
+      DatePicker,
+      SearchRoute,
     },
     data() {
         return {
-            exsampleDate: '', 
             date: null,
+            appricantEmail: '',
+            approverEmail: '',
+            reason: '',
+            from: '',
+            to: '',
+            via: [],
             roundTrip: false,
             routeFlg: false,
-            items: [
-                {
-                    price: '¥0,000',
-                    route: '×線 ◯◯駅 ~ ××線 ◯◯駅',
-                    via: '',
-                },
-                {
-                    price: '¥0,000',
-                    route: '×線 ◯◯駅 ~ ××線 ◯◯駅',
-                    via: '経由：××線 ◯◯駅',
-                },
-                {
-                    price: '¥0,000',
-                    route: '×線 ◯◯駅 ~ ××線 ◯◯駅',
-                    via: '経由：××線 ◯◯駅 , ××線 ◯◯駅',
-                },
-            ]
+            confirm: {}
         }
-    },
-    mounted() {    
-        this.exsampleDate = '例：' + this.$moment().format('YYYY/MM/DD');
     },
     methods: {
         getRouteList(){
