ec2b6ba64479bc50cc63b7d5e4da0c9d163636c7
futeri839 <e.futami.jw@gmail.com>
2020-10-20 16:03:31 +0900

    途中

--- server/api/getRouteData.js
+++ server/api/getRouteData.js
@@ -5,52 +5,54 @@
  * 
  * return : json 
  * [
- *  { price : Num },
+ *  { price : str },
  *  { via : [ str, str ] or null }
  * ]
  * When there is no data, return null.
  */
 const puppeteer = require('puppeteer');
 
-async function getViaStation(page){
-    const viaStationSelector = "#route02 > .routeDetail > .fareSection > .station > dl > dt";
-    let data = null;
-    if( await page.$(viaStationSelector).then(res => !!res) ){
-        data = await page.$eval(viaStationSelector, item => {
-        return item.textContent;
-      });
-    }
+const getTextContent = async function (page, selector){
+  let result = null;
+  if( await page.$(selector).then(res => !!res) ){
+    result = await page.$eval(selector, item => {
+      return item.textContent;
+    });
+  }
 }
 
 async function yahooTransitScraping(from, to){
-    const browser = await puppeteer.launch({
-      args: [
-        '--no-sandbox',
-        '--disable-dev-shm-usage'
-      ]
-    })
-    const page = await browser.newPage()
-    await page.goto('https://transit.yahoo.co.jp/')
-  
-    await page.type('input[name="from"]', from)
-    await page.type('input[name="to"]', to )
-    await page.select('select[name="s"]', '1')
-  
-    await page.click('#searchModuleSubmit')
-    await page.waitForNavigation()
-  
-    await page.screenshot({path: 'example.png'})
-
-    const via =  await getViaStation(page)
-    const resultData = [
-        {
-            via
-        }
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-dev-shm-usage'
     ]
-  
-    browser.close()
-    console.log(resultData)
-    return resultData
+  })
+  const page = await browser.newPage()
+  await page.goto('https://transit.yahoo.co.jp/')
+
+  await page.type('input[name="from"]', from)
+  await page.type('input[name="to"]', to )
+  await page.select('select[name="s"]', '1')
+  await page.click('#searchModuleSubmit')
+  await page.waitForNavigation()
+
+  await page.screenshot({path: 'example.png'})
+
+  const routePriceSelector = "#route02 > .routeSummary > dd > ul > .fare > .mark"
+  const price = await getTextContent(page, routePriceSelector)
+  console.log(price)
+
+  const viaStationSelector = "#route02 > .routeDetail > .fareSection > .station > dl > dt";
+  const via = await getTextContent(page, viaStationSelector)
+  const resultData = [
+      { price }, 
+      { via }
+  ]
+
+  browser.close()
+  console.log(resultData)
+  return resultData
 }
 
 module.exports = {
